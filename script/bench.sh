4cac0f7f32bc5105ab3393011cf84965a0743e7a
NoharaMasato <masato338914914@gmail.com>
2020-07-26 11:55:37 +0000

    benchをdiscordにpost install.sh修正 profilerをinstallした時に最後にパッケージ削除

--- script/bench.sh
+++ script/bench.sh
@@ -7,4 +7,4 @@ cd $BENCH_DIR
 ./bin/bench -remotes=$REMOTE_IP -output result.json
 cat result.json | jq
 cat result.json | jq ".score"
-
+cat result.json | jq ".score" | /home/isucon/isucon2020-toolbox/script/post_discord.sh
